d0d66aa49078ec61334fc43d4e0a27c812a18553
ThiagoMdO <thiago_catureba@hotmail.com>
2024-03-22 16:33:28 -0300

    first commit, initial project

---
+++ ms-products/src/test/java/com/thiagomdo/ba/challenge/msproducts/MsProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiagomdo.ba.challenge.msproducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
